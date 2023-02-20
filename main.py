ad40051f506da3c9db277d960cbbbf80cdc741ca
Eric White <led.ecodyswhite@gmail.com>
2023-02-20 15:16:20 -0800

    progression is made!

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-"""define the constraints of one deck, and the value of each card:
-how will the value of an ace be denoted in code?
-if your hand before ace + 11 > 21, ace = 1
-let's step through the logic: if the card you draw is an ace, what do you do?
-evaluate the hand you currently have; does it have an ace in it? if it has an ace in it,
-the second ace cannot equal 11.  Only one ace can equal 11."""
+import random
+"""Your current step is placing a card in the player's hand.  Good job so far!"""
 
 class Card:
     def __init__(self, suit, face, value):
@@ -32,6 +28,7 @@ def build_deck(num):
                 else:
                     deck.append(Card(suit, face, value = face))
         i += 1
+    return deck
 
 
 def ace_determination(hand):
@@ -42,17 +39,16 @@ def ace_determination(hand):
     else:
         ace.value = 11
 
-def draw_card(deck, hand):
-    """draw a card from the deck. this will produce a random card from the deck array,
-    and call on ace_determination if the card is an ace.
-    After determining if it's an ace, it will evaluate the hand for its score.
-    If there is a tie (both players' scores are 21), it will evaluate the number of cards;
-    if there is still a tie, it will evaluate the number of face cards;
-    If there is still a tie, the win goes to the house.
-    I'm pretty sure that's how blackjack works, but will double check."""
-    print("")
+def draw_card(deck):
+    """let's just try to get a random card from the deck, and make sure to remove it from the deck
+    and print it to the terminal for now.
+    Alright, we drew a card, removed it from the deck, and read it to the player. The next step
+    is to add that card to the player's hand."""
+    your_card = random.choice(deck)
+    print(f"Your card is the {your_card.face} of {your_card.suit} ")
+    deck.remove(your_card)
+    for card in playing_deck:
+        print(f"{card.face} of {card.suit}")
 
-hand = []
-build_deck(1)
-for card in deck:
-    print(f"{card.face} of {card.suit}")
+playing_deck = build_deck(1)
+draw_card(playing_deck)
